3c021d578a680212c175bbd28c2487916a65c7eb
gul fatima <gulcheema041@gmail.com>
2026-08-13 03:57:37 +0500

    Fix database initialization in tests

--- app/one.py
+++ app/one.py
@@ -2,17 +2,18 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import post, user, auth, vote
-from . import models
-from .database import engine
-
-
-models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 origins = ["*"]
 
-app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 @app.get("/")

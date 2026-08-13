cfa26cdabd8eda9151ef0159999db48e8d48f7fe
gul fatima <gulcheema041@gmail.com>
2026-08-14 02:07:05 +0500

    Fix database initialization for tests

--- app/one.py
+++ app/one.py
@@ -1,3 +1,4 @@
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -7,10 +8,12 @@ from .database import engine
 from .routers import post, user, auth, vote
 
 
-models.Base.metadata.create_all(bind=engine)
-
-
 app = FastAPI()
+
+
+@app.on_event("startup")
+def startup_event():
+    models.Base.metadata.create_all(bind=engine)
 
 
 origins = ["*"]
@@ -34,3 +37,4 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
